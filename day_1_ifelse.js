7cfae85e4bc56763aa82b0a2e9840150c0815dd8
TheZano <fauzan.habib@outlook.com>
2020-01-21 11:17:45 +0700

    adding previous day files and adding more variables to day_2_buah.html

---
+++ day_1_ifelse.js
@@ -0,0 +1,13 @@
+
+//if(number > 10) {
+//console.log('number lebih besar dari 10')
+//} else {
+//console.log('number lebih kecil dari 10')
+//}
+//
+
+let number = 9;
+
+number > 10
+  ? console.log("number lebih besar dari 10")
+  : console.log("number lebih kecil dari 10");
