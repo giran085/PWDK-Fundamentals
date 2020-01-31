22d6de481d441a9be6db6c26bd5c25ecdc0f0db2
TheZano <fauzan.habib@outlook.com>
2020-01-31 13:24:00 +0700

    edit a function and add function calls

--- day_8_dataScience.py
+++ day_8_dataScience.py
@@ -20,23 +20,45 @@ def findMedian(arr):
         return arr[int((len(arr) - 1)  / 2)]
 
 data = [71, 70, 73, 70, 70, 69, 70, 72, 71, 300, 71, 69]
-print(sortArr(data))
-sortedArr = sortArr(data)
-print(findMedian(sortedArr))
 
 def findQ3(arr):
     if (len(arr) % 2 == 0):
         endOfIndex = int(len(arr) / 2)
         return findMedian(arr[0:endOfIndex])
     else:
+        endOfIndex = int((len(arr) - 1) / 2)
         return findMedian(arr[0:])
 
 def findQ1(arr):
     if (len(arr) % 2 == 0):
         startOfIndex = int(len(arr) / 2)
         return findMedian(arr[startOfIndex:])
+    else:
+        startOfIndex = int((len(arr) -1 / 2) + 1)
+        return findMedian(arr[startOfIndex:])
 
 
+def findIQR(q1, q3):
+    return q3 - q1
 
-print(findQ3(sortedArr))
+print(sortArr(data))
+
+sortedArr = sortArr(data)
+print(findMedian(sortedArr))
 print(findQ1(sortedArr))
+print(findQ3(sortedArr))
+Q1 = findQ1(sortedArr)
+Q3 = findQ3(sortedArr)
+print(Q1, Q3)
+# The rest is trivial..
+
+def countVowel(string):
+    count = 0
+    for i in range(len(string)):
+        if ('aiueoAIUEO'.find(string[i]) > -1):
+            count += 1
+    return count
+
+
+result = countVowel("purwadhika")
+print(result)
